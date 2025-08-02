740970f07a88aa4656f7653a39a2c392b75988f4
Siddheshwar Thaware <125083847+Siddharth181920@users.noreply.github.com>
2025-08-02 22:50:54 +0530

    Delete FBS/Test directory

--- FBS/Test/test_p1.py
+++
@@ -1,9 +0,0 @@
- 
-l = int(input("Enter the length : "))
-b = int (input('Enter the breadth : '))
-r = int (input("Enter the radius : "))
-         
-area = 3.14*r**2
-
-
-print(f'Area of the figure is : {area} ')
