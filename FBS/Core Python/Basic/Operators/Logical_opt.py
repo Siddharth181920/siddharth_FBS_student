985353730e8ee502bea077a3632994452cd42de3
Siddheshwar Thaware <125083847+Siddharth181920@users.noreply.github.com>
2025-08-02 18:12:28 +0100

    Add files via upload

---
+++ FBS/Core Python/Basic/Operators/Logical_opt.py
@@ -0,0 +1,24 @@
+#1 and / Returns true if both statements are true.
+
+#x = 10
+
+#y = 15
+
+
+#print( x < 15 and y < 19)
+
+
+#2 OR / Returns the true if one statements is true.
+
+#x = 5
+#y = 10
+
+#print(x < 7 or y < 8)
+
+
+#3 NOT / Reserve the result /, retruns false if the result is true.
+
+x = 10
+y = 12
+
+print(not(x<25 and x<15 ))
---
+++ FBS/Core Python/Basic/helloWorld.py
@@ -0,0 +1,5 @@
+
+
+print("Hello World , I am happy!")
+
+print("This is my first program Date is 31 July 2025")
