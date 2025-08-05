8d0313fb44b41fbbaa4506a5d1900593830c58a7
Siddheshwar Thaware <125083847+Siddharth181920@users.noreply.github.com>
2025-08-05 23:27:38 +0530

    Delete FBS/Core Python/Basic directory

--- FBS/Core Python/Basic/IF/greater_num.py
+++
@@ -1,8 +0,0 @@
-num1 = int (input("Enter the first number : "))
-num2 = int(input("Enter the second number : "))
-
-if(num1 > num2):
-
-    print(f" {num1} is greatest Number")
-else:
-    print(f" {num2} Number is greater")
--- FBS/Core Python/Basic/helloWorld.py
+++
@@ -1,5 +0,0 @@
-
-
-print("Hello World !")
-
-print("This is my first program Date is 31 July 2025")
