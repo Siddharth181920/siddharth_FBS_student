0ce01f3c42d8eab799f44329a6c0f82c592f928e
Siddheshwar Thaware <125083847+Siddharth181920@users.noreply.github.com>
2025-08-17 13:43:35 +0530

    Delete Core Python/Assignment_4_(Looping) directory

--- Core Python/Assignment_4_(Looping)/Ass_1.py
+++
@@ -1,8 +0,0 @@
-#WAP to print all even numbers until n.
-
-num = int(input("Enter a number : "))
-
-for num in range(1,num+1):
-    
-    if(num%2==0):
-        print(num)
--- Core Python/Assignment_4_(Looping)/Ass_3.py
+++
@@ -1,7 +0,0 @@
-#WAP to print sum of series upto n.
-
-num = int(input("Enter a number : "))
-
-for num in range(1 , num +1):
-
-    print(num)
